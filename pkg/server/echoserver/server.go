22f871cc6c7e647c006cdad5396dd8836cd59d0e
Victor França Lopes <victor.franca@upper.institue>
2021-07-25 20:36:25 -0300

    Fix container parameter for echoserver (use pointer)

--- pkg/server/echoserver/server.go
+++ pkg/server/echoserver/server.go
@@ -5,7 +5,7 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-func NewEchoServer(ctn container.Container) *echo.Echo {
+func NewEchoServer(ctn *container.Container) *echo.Echo {
 	server := echo.New()
 
 	server.Logger.SetLevel(99)
