25222d570d920e4435865895f6a10d628054d09e
Ricardo Baptista <ricardogermano85@gmail.com>
2021-10-28 16:06:58 -0300

    Reverting grpc server options

--- pkg/server/grpcserver/server.go
+++ pkg/server/grpcserver/server.go
@@ -5,7 +5,6 @@ import (
 	"google.golang.org/grpc"
 )
 
-func NewGRPCServer(ctn *container.Container, serverOptions ...grpc.ServerOption) *grpc.Server {
-	options := append(serverOptions, grpc.UnaryInterceptor(MetricsInterceptor(ctn.Metrics)))
-	return grpc.NewServer(options...)
+func NewGRPCServer(ctn *container.Container) *grpc.Server {
+	return grpc.NewServer(grpc.UnaryInterceptor(MetricsInterceptor(ctn.Metrics)))
 }
