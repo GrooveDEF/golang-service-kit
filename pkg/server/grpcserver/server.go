d50bd3294c17a3f59aa636f9a11f7294e4befe89
Ricardo Baptista <ricardogermano85@gmail.com>
2021-10-28 15:32:57 -0300

    GRPC server options

--- pkg/server/grpcserver/server.go
+++ pkg/server/grpcserver/server.go
@@ -5,6 +5,7 @@ import (
 	"google.golang.org/grpc"
 )
 
-func NewGRPCServer(ctn *container.Container) *grpc.Server {
-	return grpc.NewServer(grpc.UnaryInterceptor(MetricsInterceptor(ctn.Metrics)))
+func NewGRPCServer(ctn *container.Container, serverOptions ...grpc.ServerOption) *grpc.Server {
+	options := append(serverOptions, grpc.UnaryInterceptor(MetricsInterceptor(ctn.Metrics)))
+	return grpc.NewServer(options...)
 }
